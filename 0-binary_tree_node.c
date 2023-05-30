8122b6505b6110c7e0f2003e9b28c362378dd5ae
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-05-30 00:08:39 -0700

    0x09. Web infrastructure design

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node: creates a binary tree node
+ * binary_tree_node - creates a binary tree node
  * @parent: pointer to parent node
  * @value: input value.
  * Return: pointer to new node.
